4d2d5854f0fcf1fd09fcae57e8678f9b7debf2c6
eunjin0212 <eunjin9639@gmail.com>
2020-09-03 13:12:11 +0900

    study

--- assignment.js
+++ assignment.js
@@ -4,4 +4,11 @@
 // alert(true + false);
 // alert(4 + 5 + "px");
 // alert("$" + 4 + 5);
-alert("4px" - 2);
+// alert("4px" - 2);
+
+let value = prompt("자바 스크립트의 공식 이름은 무엇일까요?");
+if (value == "ECMAScript") {
+  alert("정답입니다~");
+} else {
+  alert("모르시나요? 정답은 ECMAScript 입니다.");
+}
