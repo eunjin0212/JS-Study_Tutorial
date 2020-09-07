c6641ccf6b8b35b1fe4151f0dc80c10d6315cc10
eunjin0212 <eunjin9639@gmail.com>
2020-09-07 23:21:33 +0900

    study

--- test.js
+++ test.js
@@ -1,9 +1,49 @@
 // const a = 1;
 // const b = 2;
 // console.log(a + b);
-typeof 1;
-typeof 2.5;
-typeof "hello world";
-typeof true;
-typeof null;
-typeof undefined;
+
+// console.log(typeof 1);
+// console.log(typeof 2.5);
+// console.log(typeof "hello world");
+// console.log(typeof true);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// console.log(1 + 3);
+// console.log(2 - 4);
+// console.log(3 * 5);
+// console.log(6 / 2);
+// console.log(1 + 2 * 3);
+// console.log((1 + 2) * 3);
+
+// console.log(true || false);
+// console.log(true && false);
+// console.log(1 || 0);
+// console.log(1 && 0);
+
+// let v;
+// v = 1;
+// console.log(v);
+
+// let x = 1;
+// console.log(x);
+
+// let y = 2,
+//   z = x + y;
+// console.log(y);
+
+// const c = 1;
+// c = 2;
+// console.log(c);
+
+if (2 > 1) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let i = 0;
+while (i < 5) {
+  console.log("이 코드는 ${i + 1}번째 실행되고 있습니다.");
+  i++;
+}
