0680f25a4a7abb8ce46351a0973c9990726e5839
eunjin0212 <eunjin9639@gmail.com>
2020-09-08 19:46:41 +0900

    study

---
+++ function.js
@@ -0,0 +1,16 @@
+// function add(x, y) {
+//   const result = x + y;
+//   return result;
+// }
+
+function add(x, y) {
+  return x + y;
+}
+const result = add(2, 3);
+console.log(result);
+
+function add(a, b) {
+  return a * b;
+}
+const result1 = add(2, 2);
+console.log(result1);
--- nullundefined.js
+++ nullundefined.js
@@ -1,5 +1,41 @@
-let foo;
-console.log(foo);
+// let foo;
+// console.log(foo);
 
 const obj = {};
 console.log(obj.prop);
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+let foo;
+let bar = undefined;
+console.log(foo);
+console.log(bar);
+
+let obj1 = {};
+let obj2 = { prop: undefined };
+console.log(obj1.prop);
+console.log(obj2.prop);
+
+function printIfNotNull(input) {
+  if (input !== null && input !== undefined) {
+    console.log(input);
+  }
+}
+
+// input !== null && input !== undefined;
+// input != null;
+// input != undefined;
+
+// input === null || input === undefined;
+// input == null;
+// input == undefined;
+
+console.log(null === undefined);
+console.log(null == undefined);
+console.log(null == 1);
+console.log(null == "hello");
+console.log(null == false);
+console.log(undefined == 1);
+console.log(undefined == "hello");
+console.log(undefined == false);
