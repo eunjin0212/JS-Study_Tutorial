b1300580dd8cbde437cfbbdccc48c25a5b9782c8
eunjin0212 <eunjin9639@gmail.com>
2020-09-08 16:25:55 +0900

    study

---
+++ test2.js
@@ -0,0 +1,93 @@
+// console.log(0x4d === 77);
+// console.log(0b1001101 === 77);
+
+// Number.isInteger(1);
+// Number.isInteger(0.1);
+
+// console.log(1 + 2);
+// console.log(4 - 2);
+// console.log(5 * 6);
+// console.log(16 / 2);
+// console.log(14 % 3);
+// console.log(2 ** 3);
+
+// console.log(1 < 2);
+// console.log(3 > 4);
+// console.log(5 <= 5);
+// console.log(6 >= 5);
+// console.log(8 === 8);
+// console.log(8 !== 8);
+
+// let a = 1;
+// a++;
+// console.log(a);
+// let b = 1;
+// b++;
+// console.log(b);
+// let c = 1;
+// --c;
+// console.log(c);
+// let d = 1;
+// d--;
+// console.log(d);
+
+// let x = 0;
+// x += 1;
+// console.log(x);
+
+// let x = 0;
+// x = x + 1;
+// console.log(x);
+
+// let x = 0;
+// x -= 1;
+// console.log(x);
+
+// let x = 0;
+// x *= 2;
+// console.log(x);
+
+// let x = 4;
+// x /= 2;
+// console.log(x);
+
+// let x = 8;
+// x %= 3;
+// console.log(x);
+
+// let x = 2;
+// x **= 5;
+// console.log(x);
+
+// console.log(0.1 + 0.2);
+
+// const thisIsNaN = NaN;
+// console.log(thisIsNaN === NaN);
+
+// const thisIsNaN = NaN;
+// Number.isNaN(thisIsNaN);
+// Object.is(thisIsNaN, NaN);
+
+// console.log(0 === -0);
+// console.log(1 * -0);
+// console.log(1 + -0);
+
+// Object.is(0, -0);
+
+// console.log(1 / 0);
+// console.log(1 / -0);
+// console.log(1 / Infinity);
+// console.log(1 / -Infinity);
+
+// Number.isFinite(1);
+// Number.isFinite(Infinity);
+// Number.isFinite("1");
+// isFinite("1");
+
+// parseInt("123");
+// parseInt("110", 2);
+// parseFloat("12.345");
+// parseInt("hello");
+
+console.log(1 + null);
+console.log(1 * "1");
